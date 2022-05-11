f732da55e5bdce1c28522d3b604f671ab4eb039f
Petr Zaitsev <Petr_Zaicev@hotmail.com>
2022-05-11 14:48:22 +0300

    Create Task12.py

---
+++ 2.4 Целочисленная арифметика. Часть 1/Task12.py
@@ -0,0 +1,5 @@
+# Разделяй и властвуй
+
+# put your python code here
+num = int(input())
+print(num, num + num, (num + num) + num, ((num + num) + num) + num, (((num + num) + num) + num) + num, sep="---")
