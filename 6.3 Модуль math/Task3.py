ca7a6b088aed9608bf1d742047cffb0330cd643d
Petr Zaitsev <Petr_Zaicev@hotmail.com>
2022-05-26 20:00:12 +0300

    Create Task3.py

---
+++ 6.3 Модуль math/Task3.py
@@ -0,0 +1,11 @@
+# Средние значения
+
+
+# put your python code here
+import math
+a, b  = float(input()), float(input())
+sab, pab = a + b, a * b
+print(sab / 2)
+print(math.sqrt(pab))
+print(2 * pab / sab)
+print(math.sqrt((a**2 + b**2) / 2))     
